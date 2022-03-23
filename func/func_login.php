2812a93d32c97d3b98337a775741244a21b0fa12
aedhonk <92792552+aedhonk@users.noreply.github.com>
2022-03-23 09:37:16 +0100

    Début de l'implémentation du PHP
    Ajout de quelques fichiers de fonctions, et gestion de la connexion d'un utilisateur.
    
    Affichage du prénom dans le template de la page d'accueil.
    
    Création de template de redirection et d'erreur de connexion.
    
    Ajout d'un fichier de configuration contenant des variables privées de connexion à le BDD.

---
+++ func/func_login.php
@@ -0,0 +1,82 @@
+<?php
+include_once "func_bdd.php";
+
+
+// Objet gérant la connexion des utilisateurs
+class login
+{
+    // Déclaration d'un objet de gestion des données
+    private dataBDD $data;
+
+    public function __construct($newData)
+    {
+        $this->data = $newData;
+    }
+
+    // Gestion de la connexion
+    public function chkLogin($mail, $pass)
+    {
+        //On échappe les caractère html
+        $mail = htmlspecialchars($mail);
+        $pass = htmlspecialchars($pass);
+
+        // Si le mot de passe est vérifié
+        if ($this->verifMDP($pass, $this->data->getMDP($mail))) {
+
+            //On démarre une session
+            $this->initSession($mail);
+
+            //On redirige sur l'accueil
+            header("Location: ../index.php");
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Vérification du mot de passe entré
+    function verifMDP($mdpIN, $mdpHA)
+    {
+        // Hachage du mot de passe entré avec le poivre défini
+        $mdpIN = hash_hmac("sha256", $mdpIN, $this->data->getPepper());
+
+        //Vérification de la correspondance avec le mot de passe de la BDD
+        return password_verify($mdpIN, $mdpHA);
+    }
+
+    function hashMDP($mdpIN)
+    {
+        $mdp_h1 = hash_hmac("sha256", $mdpIN, $this->data->getPepper());
+        $mdp_h2 = password_hash($mdp_h1, PASSWORD_BCRYPT);
+
+        return $mdp_h2;
+    }
+
+    function initSession($mail)
+    {
+        //Enregistrement de l'email
+        $_SESSION['USER_MAIL'] = $mail;
+
+        //On récupère l'ID correspondant à l'email
+        $id = $this->data->getUserID($mail);
+
+        //On récupère les informations de l'utilisateur
+        $user_info = $this->data->getUser($_SESSION['USER_MAIL']);
+
+        //On récupère le nom et prénom
+        $_SESSION['USER_NAME'] = $user_info['Nom_user'];
+        $_SESSION['USER_FNAME'] = $user_info['Prenom_user'];
+
+        //On récupère le role de l'utilisateur et ses permissions
+        $_SESSION['ROLE'] = $this->data->getRole($id);
+        $_SESSION['PERM'] = $this->data->getPerm($id);
+    }
+}
+
+
+
+
+
+// ------------------------------------ CREATION DES OBJETS ----------------------------------
+$log = new login($data);
