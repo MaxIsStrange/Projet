ee6e99ad212d68f5f463d01d58430576b71f58a8
MaxIsStrange <75248506+MaxIsStrange@users.noreply.github.com>
2022-03-23 13:45:25 +0100

    finitions cartes

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
 include_once "func/func_session.php";
+include_once "func/func_bdd.php";
 
   require_once('vendor/autoload.php');
   $loader = new \Twig\Loader\FilesystemLoader('templates');
   $twig = new \Twig\Environment($loader);
+  $lastoffers = $data -> derOffres();
 
 
 if (isset($_SESSION['USER_FNAME'])) {
   echo $twig->render('accueil.html.twig', ['userfname' => $_SESSION['USER_FNAME']]);
 } else {
   $variable=12;
-  echo $twig->render('accueil.html.twig', ['userfname' => 'utilisateur', 'variable' => $variable]);
+  echo $twig->render('accueil.html.twig', ['userfname' => 'utilisateur', 'cartes' => $lastoffers]);
 }
 
 function debole($data)
