b585942b0fbb2c48906abb10710e0b4fb76f360d
MaxIsStrange <75248506+MaxIsStrange@users.noreply.github.com>
2022-03-25 13:53:58 +0100

    rajout mentions légales + footer lien mentions

---
+++ legal/index.php
@@ -0,0 +1,8 @@
+<?php
+  include_once "../func/func_session.php";
+
+  require_once('../vendor/autoload.php');
+  $loader = new \Twig\Loader\FilesystemLoader('../templates');
+  $twig = new \Twig\Environment($loader);
+
+  echo $twig->render('legal.html.twig');
