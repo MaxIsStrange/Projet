ddd0433dad91bb1346f0646631d4723970350724
Théo B <theobrunet2002@gmail.com>
2022-03-27 01:01:55 +0100

    azrqestrdytfygkuhl

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -3,23 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Récupère tous les "navbar-burger"
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
+
     // Vérifie so il y a d'autre navbar burger
     if ($navbarBurgers.length > 0) {
-  
-      // Ajout de l'action du click pour le navbar-burger
-      $navbarBurgers.forEach(el => {
-        el.addEventListener('click', () => {
-  
-          // Récupeère la cible depuis "data_target"
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
-  
-          // Déroule la classe "is-active" pour le "navbar-burger" et le "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
+
+        // Ajout de l'action du click pour le navbar-burger
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+
+                // Récupeère la cible depuis "data_target"
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Déroule la classe "is-active" pour le "navbar-burger" et le "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
         });
-      });
     }
-  });
+});
