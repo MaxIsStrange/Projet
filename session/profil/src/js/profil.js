3f9576ec8f4859d3fd7ba36978bbbeeeb89ca37e
Théo B <theobrunet2002@gmail.com>
2022-03-28 14:10:11 +0200

    zreth

---
+++ session/profil/src/js/profil.js
@@ -0,0 +1,20 @@
+const Tabs = document.querySelectorAll("[data-tab]");
+const FirstLocation = "description";
+
+window.location.hash = FirstLocation;
+
+Tabs.forEach((element) => {
+    element.addEventListener("click", (event) => {
+        let selectedTab = event.currentTarget;
+        updateActiveTab(selectedTab);
+    });
+});
+
+let updateActiveTab = (newActiveTab) => {
+    Tabs.forEach((tab) => {
+        tab.classList.remove("is-active");
+    });
+
+    newActiveTab.classList.add("is-active");
+};
+
