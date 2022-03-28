025cf8c2b9c4f154849ca47c4c90b3b4dc743e7e
Théo B <theobrunet2002@gmail.com>
2022-03-28 18:36:20 +0200

    Update msg_erreur.js

--- src/js/msg_erreur.js
+++ src/js/msg_erreur.js
@@ -1,2 +1,38 @@
-bulmaToast.toast({ message: 'Hello There' })
-bulmaToast.toast({ message: 'General Kenobi', type: 'is-danger' })
+toast({
+    message: 'Hello There',
+    type: 'is-success',
+    dismissible: true,
+    pauseOnHover: true,
+})
+
+toast({
+    message: '<h1>LOOK HERE</h1>',
+    type: 'is-danger',
+    dismissible: true,
+    pauseOnHover: true,
+    animate: { in: 'fadeIn', out: 'fadeOut' },
+})
+
+const myMessage = `It's ${new Date().toDateString()}`
+
+toast({
+    message: myMessage,
+    type: 'is-primary',
+    position: 'center',
+    closeOnClick: true,
+    pauseOnHover: true,
+    opacity: 0.8,
+})
+
+const elm = document.createElement('a')
+elm.text = 'Visit my website!'
+elm.href = 'https://rfoel.com'
+
+toast({
+    message: elm,
+    type: 'is-warning',
+    position: 'center',
+    closeOnClick: true,
+    pauseOnHover: true,
+    animate: { in: 'fadeIn', out: 'fadeOut' },
+})
