e6befc89344c07641e194b8ece3a7d3e896f4709
aedhonk <92792552+aedhonk@users.noreply.github.com>
2022-03-30 10:45:26 +0200

    Toast au bon moment page connexion

--- src/js/msg_erreur.js
+++ src/js/msg_erreur.js
@@ -1,4 +1,4 @@
-const erreur= 'Erreur de connexion l\'identifiant ou le mot de passe est incorrect';
+const erreur = 'Erreur de connexion : l\'identifiant ou le mot de passe est incorrect';
 
 bulmaToast.toast({
     type: 'is-danger',
